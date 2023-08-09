0b9e3a9e3ba9493a919e4b438bc7d74f7958a14d
Jo  Seongyun <whtjddbs0112@naver.com>
2023-08-09 10:20:37 +0900

    test end

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hello Webpack Project!",
+      template: "./src/index.ejs",
+    }),
+  ],
 };
